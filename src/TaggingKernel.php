2109c34b0fee573692de7a482864ff84a70c056a
Peter Bowyer <peter@mapledesign.co.uk>
2016-03-30 07:50:19 +0100

    Add use statement for 'HttpFoundation\Response'

--- src/TaggingKernel.php
+++ src/TaggingKernel.php
@@ -12,6 +12,7 @@
 namespace DTL\Symfony\HttpCacheTagging;
 
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\HttpKernelInterface;
 use Symfony\Component\HttpKernel\TerminableInterface;
 
